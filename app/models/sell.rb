ccea35320f4a492e6d2da5637ef69c6e7ab16c99
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-02-26 17:19:46 -0300

    Criando a relação Many Through

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,5 +1,11 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
+
+  has_many :sell_products
+  has_many :products, through: :sell_products
+
+  has_many :sell_services
+  has_many :services, through: :sell_services
   
   validates :client, presence: true
 
