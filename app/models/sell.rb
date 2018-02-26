4957973732789a9f8d2c2ea6ee9f7784cd1a2600
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-02-26 11:23:19 -0300

    Add enum status ao sell

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,10 +1,17 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
+  
+  #enum
+  enum status: { finished: 0, canceled: 1 }
 
   def fae_display_field
-    
+    id
   end
 
+  #ordena pelo id
+  def self.for_fae_index
+    order(:id)
+  end
 
   belongs_to :discount
   belongs_to :client
